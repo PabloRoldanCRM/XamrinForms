f0660febcd5a6ba761d74261391faad67eba0449
PabloRoldanCRM <pablocrp.crm@gmail.com>
2018-05-23 11:53:25 -0500

    Initial commit

---
+++ Todo/ListTaskPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Todo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListTaskPage : ContentPage
+	{
+        public string MyName { get; set; }
+        public List<TodoItem> TodoItems { get; set; }
+        public ListTaskPage ( List<TodoItem> items)
+		{
+            MyName = "Pablo";
+            BindingContext = this;
+            TodoItems = items;
+			InitializeComponent ();
+		}
+
+        public void OnSelected(object o, ItemTappedEventArgs e)
+        {
+            var toDoItems = e.Item as TodoItem;
+            DisplayAlert("Item! ", toDoItems.TaskName + " Was Selcted!", "DISSMIS");
+        }
+	}
+}
---
+++ Todo/TodoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo
+{
+   public class TodoItem
+    {
+        public string TaskName { get; set; }
+
+        public string Priority { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+
+        public bool IsDeleted { get; set; }
+
+        public TodoItem(string taskName, string priority, DateTime dueDate, bool isDeleted)
+        {
+            this.TaskName = taskName;
+            this.Priority = priority;
+            this.DueDate = dueDate;
+            this.IsDeleted = isDeleted;
+        }
+    }
+}
